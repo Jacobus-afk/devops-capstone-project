2cbff27f23575bcd6e000579222c1516b0ba9ce6
Jacobus-afk <s24162826@gmail.com>
2023-03-08 13:01:42 -0500

    added method not allowed test

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -236,3 +236,9 @@ class TestAccountService(TestCase):
             f"{BASE_URL}/{account.id}"
         )
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
+
+
+    def test_method_not_allowed(self):
+        """It should not allow an illegal method call"""
+        resp = self.client.delete(BASE_URL)
+        self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
